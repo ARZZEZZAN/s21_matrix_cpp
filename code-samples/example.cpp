2b8e705c1dec343cacd7cde777c9e6404bef5156
Administrator <admin@21-school.ru>
2021-12-29 08:01:06 +0000

    Init commit

---
+++ code-samples/example.cpp
@@ -0,0 +1,76 @@
+#include "example.h"
+
+S21Matrix::S21Matrix() {
+    _rows = 3;
+    _cols = 3;
+    //that is how allocating works in C++
+    _p = new double[_rows * _cols]();
+}
+
+/* : <attribute_name>(value) syntax helps to automatically fill attributes with 
+values*/
+S21Matrix::S21Matrix(int rows, int cols) : _rows(rows), _cols(cols) {
+    _p = new double[_rows * _cols]();
+}
+
+S21Matrix::S21Matrix(const S21Matrix& o) : _rows(o._rows), _cols(o._cols) {
+    _p = new double[o._rows * o._cols]();
+    /*standrd cpp function; copies memory of o._rows * o._cols * sizeof(double) 
+    from o.p pointer to _p pointer*/
+    //std::memcpy(_p, o._p, o._rows * o._cols * sizeof(double));
+}
+
+S21Matrix::S21Matrix(CMatrix&& o) {
+    if (_rows * _cols == o._rows * o._cols) {
+        //std::memcpy(_p, o._p, o._cols * o._rows * sizeof(double));
+    } else {
+        delete[] m_ptValues;
+        _p = new double[o._rows * o._cols]();
+        //std::memcpy(_p, o._p, o._cols * o._rows * sizeof(double));
+    }
+    _rows = o._rows;
+    _cols = o._cols;
+    delete o._p;
+    o._rows = 0;
+    o._cpls = 0;
+}
+
+S21Matrix::~S21Matrix() {
+    if (_p) {
+        //free
+        delete[] _p;
+    }
+}
+
+S21Matrix::sum_matrix(const S21Matrix& o) {
+    //exception throwing example
+    if (_rows != o._rows || _cols != o._cols) {
+        throw std::out_of_range(
+            "Incorrect input, matrices should have the same size");
+    }
+    for (auto i = 0; i < _rows + _cols; i++) {
+        _p[i] = _p[i] + o._p[i];
+    }
+}
+
+//operator oveload example
+S21Matrix S21Matrix::operator+(const S21Matrix& o) {
+    //creating result matrix
+    S21Matrix res(_rows, _cols);
+    res.sum_matrix(o);
+    return res;
+}
+
+//index operator overloads
+int& CMatrix::operator()(int row, int col) {
+    if (row >= _rows || col >= _cols) {
+        throw std::out_of_range("Incorrect input, index is out of range");
+    }
+    return _p[row * _cols + col];
+}
+int& CMatrix::operator()(int row, int col) const {
+    if (row >= _rows || col >= _cols) {
+        throw std::out_of_range("Incorrect input, index is out of range");
+    }
+    return _p[row * _cols + col];
+}
---
+++ code-samples/example.h
@@ -0,0 +1,34 @@
+#ifndef __S21MATRIX_H__
+#define __S21MATRIX_H__
+
+#include <iostream>
+
+class S21Matrix {
+private:
+    //attributes
+    int _rows, _cols;                                           // rows and columns attributes
+    double **_p;                                                // pointer to the memory where the matrix will be allocated
+
+public:
+    S21Matrix();                                                // default constructor
+    S21Matrix(int rows, int cols);                              // parameterized constructor
+    S21Matrix(const S21Matrix& o);                              // copy cnstructor
+    S21Matrix(S21Matrix&& o);                                   // move cnstructor
+    ~S21Matrix();                                               // destructor
+
+    //some operators overloads
+    S21Matrix& operator=(const S21Matrix& o);                   // assignment operator overload
+    int& operator()(int row, int col);                          // index operator overload
+    int& operator()(int row, int col) const;
+    S21Matrix& operator+=(const S21Matrix& o);                            
+    S21Matrix operator+(const S21Matrix& o);
+    S21Matrix& operator-=(const S21Matrix& o);
+    S21Matrix operator-(const S21Matrix& o);
+    
+    //some public methods
+    bool eq_matrix(const S21Matrix& o);
+    void sum_matrix(const S21Matrix& o); 
+    void sub_matrix(const S21Matrix& o);
+};
+
+#endif
